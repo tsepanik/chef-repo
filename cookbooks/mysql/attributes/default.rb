7cba44f74013033b45fe9239d9b11b8ae8fea88d
Tim Sepanik <tsepanik1@gmail.com>
2014-03-16 17:49:29 -0400

    Updated mysql
    Made updates to mysql, added template and attributes

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -1,1 +1,4 @@
-default.rb
+db_password = ""
+
+set_unless[:mysql][:server_root_password] = db_password
+set_unless[:mysql][:bind_address]         = ipaddress
--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -11,8 +11,28 @@ package "mysql-server" do
 	action :install
 end
 
+package "libapache2-mod-auth-mysql" do
+	action :install
+end
+
+package "php5-mysql" do
+	action :install
+end
+
 service "mysql" do
 	service_name "mysql"
 	supports :status => true, :restart => true, :reload => true
 	action :enable
+end
+
+template "/etc/mysql/my.cnf" do
+	source "my.cnf.erb"
+	owner"root"
+	group "root"
+	mode "0644"
+	notifies :restart, resources(:service => "mysql"), :immediately
+end
+
+service "mysql" do
+	action :start
 end
