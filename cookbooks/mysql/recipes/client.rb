0ff8e2bae38db60de41e67fbe01da6f6daee500a
tsepanik <tsepanik1@gmail.com>
2014-03-17 16:29:57 -0400

    mysql 0.4.5
    code cleanup

--- cookbooks/mysql/recipes/client.rb
+++ cookbooks/mysql/recipes/client.rb
@@ -1,39 +1,13 @@
-# package "mysql-client" do
-# 	action :install
-# end
-
-# package "ruby-mysql" do
-# 	action :install
-# end
-
-p = package "mysql-devel" do
-  package_name value_for_platform(
-    [ "centos", "redhat", "suse" ] => { "default" => "mysql-devel" },
-    "default" => 'libmysqlclient15-dev'
-  )
-  action :nothing
-end
-
-p.run_action(:install)
-
-package "mysql-client" do
-  package_name value_for_platform(
-    [ "centos", "redhat", "suse" ] => { "default" => "mysql" },
-    "default" => "mysql-client"
-  )
+package "libmysqlclient15-dev" do
   action :install
 end
 
-case node[:platform]
-when "centos","redhat", "suse"
-  package "ruby-mysql" do
-    action :install
-  end
+package "mysql-client" do
+  action :install
+end
 
-else
   r = gem_package "mysql" do
     action :nothing
   end
 
   r.run_action(:install)
-end
